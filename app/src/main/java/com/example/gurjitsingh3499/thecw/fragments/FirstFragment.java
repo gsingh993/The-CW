ff48b2ed03bbba722f6e5378f78cb11a9e2a0a86
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-03-08 13:10:32 -0800

    Adding RecyclerView

--- app/src/main/java/com/example/gurjitsingh3499/thecw/fragments/FirstFragment.java
+++ app/src/main/java/com/example/gurjitsingh3499/thecw/fragments/FirstFragment.java
@@ -3,6 +3,7 @@ package com.example.gurjitsingh3499.thecw.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,7 +62,14 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+
+
+
+        return view;
     }
 
 }
